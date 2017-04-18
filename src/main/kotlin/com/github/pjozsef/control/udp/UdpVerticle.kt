a45072654ce23264609c610b9df970764578b84b
jpollak <jozsef.pollak@emarsys.com>
2017-04-18 02:12:00 +0200

    feat(udp): add base for udp server

--- src/main/kotlin/com/github/pjozsef/control/udp/UdpVerticle.kt
+++ src/main/kotlin/com/github/pjozsef/control/udp/UdpVerticle.kt
@@ -2,9 +2,23 @@ package com.github.pjozsef.control.udp
 
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Future
+import io.vertx.core.datagram.DatagramSocketOptions
+import io.vertx.core.logging.LoggerFactory
 
-class UdpVerticle : AbstractVerticle(){
+class UdpVerticle : AbstractVerticle() {
+    val log = LoggerFactory.getLogger(this::class.java)
+
     override fun start(startFuture: Future<Void>) {
-        startFuture.complete()
+        val opts = DatagramSocketOptions().setBroadcast(true)
+        val socket = vertx.createDatagramSocket(opts)
+        socket.listen(config().getInteger("port", 5000), "0.0.0.0", { asyncResult ->
+            if (asyncResult.succeeded()) {
+                socket.handler({ packet ->
+                    log.info("got: ${packet.data()}")
+                })
+            } else {
+                println("Listen failed ${asyncResult.cause()}")
+            }
+        })
     }
 }
