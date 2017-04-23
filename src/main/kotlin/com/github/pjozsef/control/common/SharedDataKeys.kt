31e041d159a669294fb17fbb03feaa088dfb7156
jpollak <jozsef.pollak@emarsys.com>
2017-04-23 13:00:53 +0200

    feat(service-discovery): udp verticle serves info for service discovery

---
+++ src/main/kotlin/com/github/pjozsef/control/common/SharedDataKeys.kt
@@ -0,0 +1,8 @@
+package com.github.pjozsef.control.common
+
+object SharedDataKeys {
+    val webInfo = "WEB_INFO"
+    val webInfoName = "WEB_INFO_NAME"
+    val webInfoOS = "WEB_INFO_OS"
+    val webInfoPort = "WEB_INFO_PORT"
+}
--- src/main/kotlin/com/github/pjozsef/control/udp/UdpVerticle.kt
+++ src/main/kotlin/com/github/pjozsef/control/udp/UdpVerticle.kt
@@ -1,5 +1,6 @@
 package com.github.pjozsef.control.udp
 
+import com.github.pjozsef.control.common.SharedDataKeys
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Future
 import io.vertx.core.datagram.DatagramSocketOptions
@@ -9,18 +10,41 @@ class UdpVerticle : AbstractVerticle() {
     val log = LoggerFactory.getLogger(this::class.java)
 
     override fun start(startFuture: Future<Void>) {
+        val token = config().getString("token")
+
         val opts = DatagramSocketOptions().setBroadcast(true)
         val socket = vertx.createDatagramSocket(opts)
-        socket.listen(config().getInteger("port"), "0.0.0.0", { asyncResult ->
+        val port = config().getInteger("port")
+        socket.listen(port, "0.0.0.0", { asyncResult ->
             if (asyncResult.succeeded()) {
                 startFuture.complete()
+                log.info("Udp server listening on $port")
                 socket.handler({ packet ->
-                    log.info("got: ${packet.data()}")
+                    val senderPort = packet.sender().port()
+                    val senderHost = packet.sender().host()
+                    val message = packet.data().toString()
+                    if (message == token) {
+                        log.info("Valid request from $senderHost")
+                        socket.send(createReply(), senderPort, senderHost, {})
+                    } else {
+                        log.info("Invalid request: $message, from $senderHost")
+                    }
                 })
             } else {
                 startFuture.fail(asyncResult.cause())
                 println("Listen failed ${asyncResult.cause()}")
             }
         })
+    }
+
+    private fun createReply(): String {
+        val sd = vertx.sharedData()
+        val webInfo = sd.getLocalMap<String, String>(SharedDataKeys.webInfo)
+
+        val name = webInfo.get(SharedDataKeys.webInfoName)
+        val os = webInfo.get(SharedDataKeys.webInfoOS)
+        val port = webInfo.get(SharedDataKeys.webInfoPort)
+
+        return "$name,$os,$port"
     }
 }
