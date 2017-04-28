3e1cac58d9303aafa50c4f031454d739b3673602
jpollak <jozsef.pollak@emarsys.com>
2017-04-28 22:18:28 +0200

    feat(commander): add osx implementation

--- src/main/kotlin/com/github/pjozsef/control/command/platform/OsxCommander.kt
+++ src/main/kotlin/com/github/pjozsef/control/command/platform/OsxCommander.kt
@@ -1,49 +1,68 @@
 package com.github.pjozsef.control.command.platform
 
 import com.github.pjozsef.control.command.OS
+import io.vertx.core.logging.LoggerFactory
 
 class OsxCommander(os: OS) : BaseCommander(os) {
 
+    val log = LoggerFactory.getLogger(this::class.java)
+
     override val supported: List<String>
-        get() = listOf()
+        get() = listOf(
+                "shutdown",
+                "restart",
+                "suspend",
+                "playpause",
+                "next",
+                "prev",
+                "mute",
+                "volup",
+                "voldown",
+                "setvolume")
 
     override fun shutDown() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        exec("osascript -e 'tell app \"System Events\" to shut down'", log)
     }
 
     override fun restart() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        exec("osascript -e 'tell app \"System Events\" to restart'", log)
     }
 
     override fun suspend() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        exec("osascript -e 'tell app \"System Events\" to sleep'", log)
     }
 
     override fun playPause() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        exec("osascript -e 'tell application \"Spotify\" to playpause'", log)
     }
 
     override fun next() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        exec("osascript -e 'tell application \"Spotify\" to next track'", log)
     }
 
     override fun prev() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        exec("osascript -e 'tell application \"Spotify\" to previous track'", log)
     }
 
     override fun mute() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        exec("osascript " +
+                "-e 'set curVolume to get volume settings' " +
+                "-e 'if output muted of curVolume is false then' " +
+                "-e 'set volume with output muted' " +
+                "-e 'else' " +
+                "-e 'set volume without output muted' " +
+                "-e 'end if'", log)
     }
 
     override fun volUp() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        exec("osascript -e 'set volume output volume (output volume of (get volume settings) + 10)'", log)
     }
 
     override fun volDown() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        exec("osascript -e 'set volume output volume (output volume of (get volume settings) - 10)'", log)
     }
 
     override fun setVolume(level: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        exec("osascript -e 'set volume output volume 100'", log)
     }
 }
