3af22a051cd4d5b602cc0e4129a2b1ec95e4340a
jpollak <jozsef.pollak@emarsys.com>
2017-04-29 21:07:04 +0200

    feat(commander): add non-groovy implementation of executing processes

--- src/main/kotlin/com/github/pjozsef/control/command/platform/BaseCommander.kt
+++ src/main/kotlin/com/github/pjozsef/control/command/platform/BaseCommander.kt
@@ -2,13 +2,30 @@ package com.github.pjozsef.control.command.platform
 
 import com.github.pjozsef.control.command.OS
 import io.vertx.core.logging.Logger
+import java.io.InputStream
+
+typealias CommandOutput = Pair<String, String>
+
 
 abstract class BaseCommander(override val platform: OS) : Commander {
-    private val runner = CommandRunner()
 
-    protected fun exec(command: String, log: Logger) {
-        runner.exec(command, log)
+    protected fun exec(commandArray: Array<String>, log: Logger): CommandOutput {
+        log.info("Executing ${commandArray.joinToString(" ")}")
+        val process = ProcessBuilder(*commandArray).start()
+        val (out, err) = process.output()
+        if (out.isNotBlank()) {
+            log.info(out)
+        }
+        if (err.isNotBlank()) {
+            log.error(err)
+            throw Exception(err)
+        }
+        return out to err
     }
+
+    private fun Process.output() = inputStream.readAll().trim() to errorStream.readAll().trim()
+
+    private fun InputStream.readAll() = bufferedReader().use { it.readText() }
 
     protected fun normalize(level: Int) =
             if (level < 0) 0
